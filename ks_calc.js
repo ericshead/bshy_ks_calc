947493d5a13a6ad65d2325a554cb98e64e7712ee
EricR <70651771+ericshead@users.noreply.github.com>
2021-09-14 00:52:37 -0400

    Change from TM to SSolo3
    Initial changes; need KS to launch to confirm pricing, shipping, etc.

--- ks_calc.js
+++ ks_calc.js
@@ -3,6 +3,7 @@ function calcTotalPledge() {
 	var elem;
 	var bgotm = false;
 	var shippingIncl = true;
+	var usonlycheck = false;
 	
 	/* calculating the total */
 	elem = document.getElementById("Reward");
@@ -15,7 +16,14 @@ function calcTotalPledge() {
 	var ksAmount = totalAmount -1;
 	
 	if (bgotmDiscount != "0") { bgotm = true; }
-	if (rewardValue > 3 && shippingCost == 0) { shippingIncl = false; }
+	if (rewardValue > 3 && shippingCost == 0) { shippingIncl = false; }  /* PnP shipping tier check */
+	
+	/* Unsurmountable: if MEGA-fan tier selected; force shipping cost */
+	if (rewardValue == 50 && shippingCost != 9) { document.getElementById("Shipping").value = 9; }
+	/* Unsurmountable: if MEGA-fan shipping tier selected, but reward level not; force shipping to standard US */
+	if (rewardValue != 50 && shippingCost == 9) { document.getElementById("Shipping").value = 5; }
+	
+	if (rewardValue >= 50) { usonlycheck = true; }  /* checking for MEGA-fan or retail level pledges */
 	
 	/* displaying the result on the page */
 	if (shippingIncl) {
@@ -23,8 +31,16 @@ function calcTotalPledge() {
 	
 		if (bgotm) {
 			document.getElementById("oneDollar").innerHTML = "$1 Reward Tier \+  &ldquo;Bonus Support&rdquo; = <strong>$"+ ksAmount +".00</strong>";
+			document.getElementById("showDiscount").style.marginTop = "0.6em";
 			document.getElementById("showDiscount").innerHTML = "Total discount = <strong>$"+ bgotmDiscount +".00</strong>";
+		} else {
+			document.getElementById("oneDollar").innerHTML = "";
+			document.getElementById("showDiscount").style.marginTop = "0";
+			document.getElementById("showDiscount").innerHTML = "";
 		}
+		if (usonlycheck) {
+			document.getElementById("usonlywarning").innerHTML = "NOTE: this reward tier can only be shipped within the United States!"
+		} else { document.getElementById("usonlywarning").innerHTML = "" }
 	}
 	else {
 		document.getElementById("showAmount").innerHTML = "Please select a shipping destination.";
@@ -38,6 +54,12 @@ function checkPnP () {
 	}
 	else if (parseInt(elem.options[elem.selectedIndex].value) == 0) {
 		document.getElementById("Shipping").value = 0;
+		document.getElementById("oneDollar").innerHTML = "";
+		document.getElementById("Discount").value = 0;
+		document.getElementById("showDiscount").innerHTML = "";
+	}
+	else if (parseInt(elem.options[elem.selectedIndex].value) >= 50) {
+		document.getElementById("Shipping").value = 9;
 		document.getElementById("oneDollar").innerHTML = "";
 		document.getElementById("Discount").value = 0;
 		document.getElementById("showDiscount").innerHTML = "";
